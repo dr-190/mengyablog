bac585755d7ea63554442d186192ccf63c6f20a3
dr-190 <syybili9@gmail.com>
2024-02-02 14:41:32 +0800

    Site updated: 2024-02-02 14:41:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/gybz/"];function toRandomPost(){
+var posts=["/posts/1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
