c0f05decd90de34309fe5aed675b72d30a9abb54
dr-190 <syybili9@gmail.com>
2024-02-03 12:08:32 +0800

    Site updated: 2024-02-03 12:08:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/8bd69e52.html"];function toRandomPost(){
+var posts=["posts/8b8a8c83.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
