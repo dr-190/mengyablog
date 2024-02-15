ab9c91a6ee01f175591f1cfd3291890ee31daafb
dr-190 <syybili9@gmail.com>
2024-02-15 19:06:23 +0800

    Site updated: 2024-02-15 19:06:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/8b8a8c83.html","posts/7c6f7035.html","posts/c6d88a0.html"];function toRandomPost(){
+var posts=["posts/8b8a8c83.html","posts/c6d88a0.html","posts/7c6f7035.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
